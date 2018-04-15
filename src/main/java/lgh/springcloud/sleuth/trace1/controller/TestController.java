adafd5b4ac8a43799ab99fada54ad9226d22efc8
Johnson Lau <gatement@gmail.com>
2018-04-15 22:23:36 +0800

    go

---
+++ src/main/java/lgh/springcloud/sleuth/trace1/controller/TestController.java
@@ -0,0 +1,23 @@
+package lgh.springcloud.sleuth.trace1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class TestController {
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping(value="/trace-1", method=RequestMethod.GET)
+	public String trace() {
+		log.info("=== call trace-1 ===");
+		return restTemplate.getForEntity("http://trace-2/trace-2", String.class).getBody();
+	}
+
+}
---
+++ src/test/java/lgh/springcloud/sleuth/trace1/SpringcloudSleuthTrace1ApplicationTests.java
@@ -0,0 +1,16 @@
+package lgh.springcloud.sleuth.trace1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudSleuthTrace1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
